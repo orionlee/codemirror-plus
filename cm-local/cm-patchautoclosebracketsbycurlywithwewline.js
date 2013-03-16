b6852efce85db651b463ad5a046c1e6573bceb20
Sam Lee <orionlee@yahoo.com>
2013-03-15 17:45:34 -0700

    auto close curly bracket: improve end comment handling.

--- cm-local/cm-patchautoclosebracketsbycurlywithwewline.js
+++ cm-local/cm-patchautoclosebracketsbycurlywithwewline.js
@@ -1,9 +1,8 @@
 
 (function() {
-
   CodeMirror.defineOption("patchAutoCloseBracketsByCurlyWithNewline", false, function(cm, val, old) {
     var wasOn = old && old != CodeMirror.Init;
-    if (val && !wasOn) {
+    if (val) {
       var patched = patchAutoCloseBracketsByCurlyWithNewline(cm);
       if (!patched) {
         if (console && console.warn) 
@@ -14,8 +13,9 @@
         console.error('CodeMirror patchAutoCloseBracketsByCurlyWithNewline: once patched, cannot be undone.');
     }
   });
+  
 
-	/**
+  /**
 	 * Pos cases;
 	 *     function foo(bar) {
    *     function foo() {
@@ -24,17 +24,21 @@
    *     foo = function(bar) {
    *     var foo = (function(bar) {
    *     (function(bar) {
+   * Pos cases (end comma, no newline)
+   *     bar = {}; 
+   *     var bar = {}; 
+   *     window.bar = {}; 
    * Neg cases (no end comment):
 	 *     callFun(bar, function() {
 	 *     var res = callFun(bar, function() {
 	 *     for (i = 0; i < foo; i++ ) {
    *     if (foo < bar) {
-   * TODO: handle cases where close curly does not skip line
-   *     // fooo {} (the line is a comment line)
-   *     bar = {}; // MAYBE object
+   * 
+   * Cases where current line is a comment is handled out of scope
    */  
-  function genJsCloseCurlyComment(line) {
-    var matched, comment;
+  function genJsCloseCurlyPosAndComment(line) {
+    var isCloseCurlyNewline = true; // default
+    var comment;
     
     // form function foobar(maybeVar) {
     var reFuncNamed = /^\s*function\s+([^(\s]+)\s*\(([^)]*)\)/ ;
@@ -42,47 +46,67 @@
     // form  var bar = function(maybeVar) { .. expected closing line ;
     // or form  bar = function(maybeVar) { .. expected closing line ;
     // or form  window.bar = function(maybeVar) { .. expected closing line ;
-    var reFuncAnonyWithVar = /^\s*([^=,\s]+)\s*=\s*function\s*\(([^)]*)\)/ ;
+    var reFuncAnonyWithVar = /^\s*(?:var\s+)?([^=,\s]+)\s*=\s*function\s*\(([^)]*)\)/ ;
 
     // form  var bar = (function(maybeVar) { ... expected closing line: }();
-    var reFuncAnonyApplyWithVar = /^\s*var\s+([^=\s]+)\s*=\s*\(function\s*\(([^)]*)\)/ ;
+    var reFuncAnonyApplyWithVar = /^\s*(?:var\s+)?([^=\s]+)\s*=\s*\(function\s*\(([^)]*)\)/ ;
         
     // form  (function(maybeVar) { ... expected closing line: }();
     var reFuncAnonyApplyNoVar = /^\s*\(function\s*\(([^)]*)\)/ ;
 
+    var reObjCreation = /^\s*(?:var\s+)?([^=,\s]+)\s*=\s*$/; // about to type { at the end of line
+
+    var matched;
     if (matched = line.match(reFuncNamed)) {
       comment = " // function " + matched[1] + "(" + (!matched[2] ? "" : "..") + ")";
     } else if (matched = line.match(reFuncAnonyWithVar)) {
       comment = "; // " + matched[1] + " = function(" + (!matched[2] ? "" : "..") + ")";
     } else if (matched = line.match(reFuncAnonyApplyWithVar)) {
-      comment = ")(); // var " + matched[1] + " = (function(" + (!matched[2] ? "" : "..") + ")";
+      comment = ")(); // " + matched[1] + " = (function(" + (!matched[2] ? "" : "..") + ")";
     } else if (matched = line.match(reFuncAnonyApplyNoVar)) {
       comment = ")(); // (function(" + (!matched[1] ? "" : "..") + ")";
+    } else if (matched = line.match(reObjCreation)) {
+      isCloseCurlyNewline = false;
+      comment = ";";  // not exactly comment but the comma is helpful for the end user.
     } else {
       comment = "";
     }
     
-    return comment;
-  } // function genJsCloseCurlyComment(..)
+    return {
+      isCloseCurlyNewline: isCloseCurlyNewline, 
+      comment: comment 
+    };
+  } // function genJsCloseCurlyPosAndComment(..)
 
   // the main runtime logic
   function autoCloseCurly(cm) {
     var left='{', right ='}';
     // should be in closure
-    var cur = cm.getCursor("start"), ahead = CodeMirror.Pos(cur.line + 2, 0);
-    var anchor =  CodeMirror.Pos(cur.line + 1, 0);
-    var outStr = left + '\n\n' + right;
+    var cur = cm.getCursor("start");
+    var state = genJsCloseCurlyPosAndComment(cm.getLine(cur.line));
+    // handle if close curly should be at a new line
+    var ahead, anchor, outstr;    
+    if (state.isCloseCurlyNewline) {
+      ahead = CodeMirror.Pos(cur.line + 2, 0);
+      anchor =  CodeMirror.Pos(cur.line + 1, 0);
+      outStr = left + '\n\n' + right;
+    } else {
+      ahead = CodeMirror.Pos(cur.line, cur.ch + 1);
+      anchor =  ahead;
+      outStr = left + right;      
+    }
     
-    var endComment = genJsCloseCurlyComment(cm.getLine(cur.line));
-    if (endComment) {
-      outStr += endComment;
-      ahead = CodeMirror.Pos(ahead.line, endComment.length + 1 - 1); // needed to include } but zero-based
+    if (state.comment) {
+      outStr += state.comment;
+      ahead = CodeMirror.Pos(ahead.line, state.comment.length + 1 - 1); // needed to include } but zero-based
     } 
     cm.replaceSelection(outStr, { head: ahead, anchor: anchor });
-    cm.setCursor(ahead);
-    cm.execCommand('indentAuto');
-    cm.setCursor(anchor);
-    cm.execCommand('indentAuto');
+    if (state.isCloseCurlyNewline) {
+      cm.setCursor(ahead);
+      cm.execCommand('indentAuto');
+      cm.setCursor(anchor);
+      cm.execCommand('indentAuto');
+    }
   } // function autoCloseCurly(..)
   
   // setup logic to patch the keyMap generated by autocloseBrackets
@@ -103,5 +127,4 @@
     return patched;
   }
 })();
-
 
